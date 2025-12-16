a0a5a3b5caea4301f6e04a3c340dfaa632a07dcd
hjhjhj0917 <abc22842199@gmail.com>
2025-12-16 14:33:21 +0900

    융합프로젝트 메인페이지 작업 시작 hero 이미지 생성

--- src/main/java/com/example/example/controller/HomeController.java
+++ src/main/java/com/example/example/controller/HomeController.java
@@ -3,11 +3,9 @@ package com.example.example.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @Slf4j
-@RequestMapping(value = "/home")
 public class HomeController {
 
     @GetMapping(value = "main")
