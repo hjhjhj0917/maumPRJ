50d495f21476271f3299dce351fe51532dc2bdb5
hjhjhj0917 <abc22842199@gmail.com>
2025-12-21 19:27:19 +0900

    융합프로젝트 회원가입 ui 수정

--- src/main/resources/static/js/account/register.js
+++ src/main/resources/static/js/account/register.js
@@ -2,6 +2,7 @@ let currentStep = 1;
 
 $(document).ready(function() {
     updateStepper(1);
+    updateButtons(1);
 });
 
 function nextStep(step) {
@@ -14,9 +15,21 @@ function prevStep(step) {
 
 function showStep(step) {
     currentStep = step;
+
     let translateValue = (step - 1) * -100;
     $('#slideTrack').css('transform', 'translateX(' + translateValue + '%)');
+
+    $('.form-step').removeClass('active-slide');
+    $('#step' + step).addClass('active-slide');
+
+    updateButtons(step);
+
     updateStepper(step);
+}
+
+function updateButtons(step) {
+    $('.btn-step-group').removeClass('active');
+    $('#btns' + step).addClass('active');
 }
 
 function updateStepper(step) {
