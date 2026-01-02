b6ca0cc5629fb51d3eeb6fe2d275bac3e57eda5c
hjhjhj0917 <abc22842199@gmail.com>
2026-01-02 19:56:49 +0900

    융합프로젝트 일기 작성 페이지 추가

---
+++ src/main/java/com/example/example/controller/DiaryController.java
@@ -0,0 +1,23 @@
+package com.example.example.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+@RequestMapping(value = "/diary")
+@RequiredArgsConstructor
+public class DiaryController {
+
+    @GetMapping(value = "write")
+    public String write() {
+
+        log.info("{}.write Start!", this.getClass().getName());
+        log.info("{}.write End!", this.getClass().getName());
+
+        return "diary/write";
+    }
+}
