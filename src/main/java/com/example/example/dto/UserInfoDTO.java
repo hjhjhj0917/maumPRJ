8ca071540b768c37d99d9b32e8bb1e44ab6da4ee
hjhjhj0917 <abc22842199@gmail.com>
2026-01-24 18:42:02 +0900

    융합프로젝트

--- src/main/java/com/example/example/dto/UserInfoDTO.java
+++ src/main/java/com/example/example/dto/UserInfoDTO.java
@@ -6,7 +6,13 @@ import lombok.Setter;
 @Getter
 @Setter
 public class UserInfoDTO {
+
     private String userName;
     private String userId;
     private String password;
+    private String email;
+    private String address;
+    private String birth;
+    private String profileImg;
+    private String nickname;
 }
