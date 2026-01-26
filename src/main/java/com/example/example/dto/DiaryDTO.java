76c8191fd1aec3c23762a9ada10d9317d40f9bde
hjhjhj0917 <abc22842199@gmail.com>
2026-01-26 23:10:51 +0900

    융합프로젝트

---
+++ src/main/java/com/example/example/dto/DiaryDTO.java
@@ -0,0 +1,13 @@
+package com.example.example.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DiaryDTO {
+
+    private String title;
+    private String content;
+    private String date;
+}
