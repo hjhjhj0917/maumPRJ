c282f1cac5847116365e7863287518a4f6bb9786
hjhjhj0917 <abc22842199@gmail.com>
2026-02-23 21:00:23 +0900

    융합프로젝트

---
+++ src/main/java/com/example/example/controller/MailController.java
@@ -0,0 +1,10 @@
+package com.example.example.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+
+@Controller
+@Slf4j
+public class MailController {
+}
