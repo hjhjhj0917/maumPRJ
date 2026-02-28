8ea2cea962fde770225b989ddf347e26b9873418
hjhjhj0917 <abc22842199@gmail.com>
2026-02-28 20:30:34 +0900

    융합프로젝트

--- src/main/java/com/example/example/service/impl/MailService.java
+++ src/main/java/com/example/example/service/impl/MailService.java
@@ -1,4 +1,11 @@
 package com.example.example.service.impl;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
 public class MailService {
 }
